4f28b259bd558455938fd581ac422cd4438f89e0
Pratul Kumar <pratulkumar2003@gmail.com>
2025-10-06 12:55:27 +0530

    Added R2 Accuracy and plot a graph to see the difference between Actual and predicted

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
 
 
 data={
@@ -12,16 +14,16 @@ data={
 
 df=pd.DataFrame(data)
 
-print(df.head())
+# print(df.head())
 
-plt.scatter(df["Hours_Studied"],df["Score"])
-plt.title("Hours Studied vs Score")
-plt.xlabel("Hours Studied")
-plt.ylabel("Score")
-plt.show()
+# plt.scatter(df["Hours_Studied"],df["Score"])
+# plt.title("Hours Studied vs Score")
+# plt.xlabel("Hours Studied")
+# plt.ylabel("Score")
+# plt.show()
 
-X=df[["Hours_Studied"]]
-y=df["Score"]
+X=df[["Hours_Studied"]].values
+y=df["Score"].values
 
 X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.2,random_state=42)
 
@@ -31,4 +33,15 @@ pred=model.predict(X_test)
 print("Predictions: ",pred)
 print("Actual: ",list(y_test))
 print("Predicted Score for 9 hours: ",model.predict([[9]])[0])
+
+score=r2_score(y_test, pred)
+print("R2 Accuracy: ",round(score, 2))
+
+plt.scatter(X, y, label="Actual")
+plt.plot(X, model.predict(X), color='red', label="Predicted line")
+plt.legend()
+plt.show()
+
+
+
 
