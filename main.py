46dda1609425bc04c0d51f6173630e0eb116afd3
Pratul Kumar <pratulkumar2003@gmail.com>
2025-10-06 12:33:24 +0530

    Added some Visualiztion code and Train the model and after that done a predictions

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 
 data={
     "Hours_Studied":[1,2,3,4,5,6,7,8,9,10],
@@ -9,3 +13,22 @@ data={
 df=pd.DataFrame(data)
 
 print(df.head())
+
+plt.scatter(df["Hours_Studied"],df["Score"])
+plt.title("Hours Studied vs Score")
+plt.xlabel("Hours Studied")
+plt.ylabel("Score")
+plt.show()
+
+X=df[["Hours_Studied"]]
+y=df["Score"]
+
+X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+model=LinearRegression()
+model.fit(X_train, y_train)
+pred=model.predict(X_test)
+print("Predictions: ",pred)
+print("Actual: ",list(y_test))
+print("Predicted Score for 9 hours: ",model.predict([[9]])[0])
+
